4d97d4f8d02a6e26fd679fa116e6ca2bc9636519
diegoalrv <d.alexis.ramirez@gmail.com>
2023-12-19 23:53:32 -0300

    working

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import os
+import paho.mqtt.publish as publish
+
+app = Flask(__name__)
+mqtt_broker = os.getenv('MQTT_BROKER', 'localhost')
+mqtt_port = int(os.getenv('MQTT_PORT', 1883))
+
+@app.route('/post-image', methods=['POST'])
+def post_image():
+    image_url = request.json.get('url')
+    publish.single("proyectores", image_url, hostname=mqtt_broker, port=mqtt_port)
+    return {"message": "Image URL sent to MQTT broker."}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
